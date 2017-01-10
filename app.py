0f761f784cc8587c20477e0a21acd1509078bd18
Deepak-89 <deepuisdon@gmail.com>
2017-01-10 19:27:46 +0530

    Update app.py

--- app.py
+++ app.py
@@ -96,8 +96,4 @@ def log(message):  # simple wrapper for logging to stdout on heroku
     sys.stdout.flush()
     
 if __name__ == '__main__':
-    port = int(os.getenv('PORT', 5000))
-
-    print "Starting app on port %d" % port
-
-    app.run(debug=False, port=port, host='0.0.0.0')
+    app.run(debug=True)
